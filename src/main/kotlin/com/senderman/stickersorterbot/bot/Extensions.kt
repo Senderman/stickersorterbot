1385cde3c96f05cc96a539451b8f7d4e45a7b632
Fyodor Doletov <doletov.fyodor@yandex.ru>
2020-07-26 16:47:41 +0300

    wtf2

--- src/main/kotlin/com/senderman/stickersorterbot/bot/Extensions.kt
+++ src/main/kotlin/com/senderman/stickersorterbot/bot/Extensions.kt
@@ -19,3 +19,16 @@ fun CommonAbsSender.sendMessage(
         .enableHtml()
         .setWebPagePreviewEnabled(enableWebPagePreview)
         .call(this)
+
+fun CommonAbsSender.getFirstNameById(userId: Int): String {
+    val member = Methods.getChatMember()
+            .setChatId(userId.toLong())
+            .setUserId(userId)
+            .call(this)
+    val user = member.user ?: return "Без имени"
+    val name: String = if (user.firstName.isBlank()) "Без имени" else user.firstName
+    return name
+            .replace("<", "&lt;")
+            .replace(">", "&gt;")
+            .replace("&", "&amp;")
+}
--- src/main/kotlin/com/senderman/stickersorterbot/web/controller/MainPageController.kt
+++ src/main/kotlin/com/senderman/stickersorterbot/web/controller/MainPageController.kt
@@ -1,5 +1,7 @@
 package com.senderman.stickersorterbot.web.controller
 
+import com.annimon.tgbotsmodule.services.CommonAbsSender
+import com.senderman.stickersorterbot.bot.getFirstNameById
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
@@ -8,11 +10,16 @@ import java.security.Principal
 
 @Controller
 @RequestMapping("/", "main")
-class MainPageController {
+class MainPageController(
+        private val telegram:CommonAbsSender
+) {
 
     @GetMapping
-    fun showMainPage(principal: Principal, model: Model) {
-
+    fun showMainPage(principal: Principal, model: Model):String {
+        val userId = principal.name.toInt()
+        val username = telegram.getFirstNameById(userId)
+        model.addAttribute("username", username)
+        return "main"
     }
 
 }
